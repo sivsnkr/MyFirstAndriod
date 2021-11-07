bed8605e638d45fb6669f34c8e34c1800733d077
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-07 08:48:07 +0530

    add a reset button && final commit

--- app/src/main/java/com/example/myfirstandriod/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandriod/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        val countUpButton : Button = findViewById(R.id.count_up)
+        countUpButton.setOnClickListener{countUp()}
+        val resetButton : Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{resetValue()}
+
     }
 
     private fun rollDice(){
@@ -20,5 +26,25 @@ class MainActivity : AppCompatActivity() {
         val resultText : TextView = findViewById<TextView>(R.id.display_text)
         val randomFace = (1..6).random()
         resultText.text = randomFace.toString()
+    }
+
+    private fun countUp(){
+        val resultText : TextView = findViewById(R.id.display_text)
+        val text = resultText.text.toString()
+        val finalText : String = if(text.isDigitsOnly()){
+            var value = text.toInt()
+            if(value < 6)
+                value++
+            value.toString()
+        }else{
+            "1"
+        }
+
+        resultText.text = finalText
+    }
+
+    private fun resetValue(){
+        val resultText : TextView = findViewById(R.id.display_text)
+        resultText.text = "Hello World!"
     }
 }
