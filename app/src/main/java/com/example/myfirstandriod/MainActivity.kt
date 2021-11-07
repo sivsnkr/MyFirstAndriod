5a25669bda20a67a4faaa5df41641237742a4803
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-07 11:00:38 +0530

    show random dice face image in place of digits

--- app/src/main/java/com/example/myfirstandriod/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandriod/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.isDigitsOnly
 
@@ -13,37 +14,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
-        val countUpButton : Button = findViewById(R.id.count_up)
-        countUpButton.setOnClickListener{countUp()}
-        val resetButton : Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener{resetValue()}
+//        val countUpButton : Button = findViewById(R.id.count_up)
+//        countUpButton.setOnClickListener{countUp()}
+//        val resetButton : Button = findViewById(R.id.reset_button)
+//        resetButton.setOnClickListener{resetValue()}
 
     }
 
     private fun rollDice(){
-        val resultText : TextView = findViewById(R.id.display_text)
-        val randomFace = (1..6).random()
-        resultText.text = randomFace.toString()
-    }
-
-    private fun countUp(){
-        val resultText : TextView = findViewById(R.id.display_text)
-        val text = resultText.text.toString()
-        val finalText : String = if(text.isDigitsOnly()){
-            var value = text.toInt()
-            if(value < 6)
-                value++
-            value.toString()
-        }else{
-            "1"
+        val resultImageView : ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when((1..6).random()){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
 
-        resultText.text = finalText
+        resultImageView.setImageResource(drawableResource)
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun resetValue(){
-        val resultText : TextView = findViewById(R.id.display_text)
-        resultText.text = "Hello World!"
-    }
+//    private fun countUp(){
+//        val resultText : TextView = findViewById(R.id.display_text)
+//        val text = resultText.text.toString()
+//        val finalText : String = if(text.isDigitsOnly()){
+//            var value = text.toInt()
+//            if(value < 6)
+//                value++
+//            value.toString()
+//        }else{
+//            "1"
+//        }
+//
+//        resultText.text = finalText
+//    }
+//
+//    @SuppressLint("SetTextI18n")
+//    private fun resetValue(){
+//        val resultText : TextView = findViewById(R.id.display_text)
+//        resultText.text = "Hello World!"
+//    }
 }
