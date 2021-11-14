7abc1312e4801b25f4b9262b242eb04b8b177ce4
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-14 07:24:56 +0530

    set clickHandler to change nickname

--- app/src/main/java/com/example/myfirstandriod/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandriod/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myfirstandriod
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        (findViewById<Button>(R.id.done_button)).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view : View){
+        val editText : EditText = findViewById(R.id.nickname_edit)
+        val nicknameTextview : TextView = findViewById(R.id.nickname_text)
+        nicknameTextview.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextview.visibility = View.VISIBLE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
